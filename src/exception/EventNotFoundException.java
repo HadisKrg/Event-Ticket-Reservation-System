c3bf826aab013a92d87b9079f0153ce4603a696d
Hadis <hadiskrg2005@gmail.com>
2026-07-12 23:03:15 +0330

    feat(exception): add EventNotFoundException exception

---
+++ src/exception/EventNotFoundException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class EventNotFoundException extends RuntimeException {
+    public EventNotFoundException(String message,Throwable cause) {
+        super(message,cause);
+    }
+}
