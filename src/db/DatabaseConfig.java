764469918dfa616d7bd63620bfdc27016dedd6d0
Hadis <hadiskrg2005@gmail.com>
2026-07-12 21:53:03 +0330

    feat(db): add DatabaseConfig class

---
+++ src/db/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package db;
+
+import exception.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String url="jdbc:postgresql://localhost:5432/event_ticket";
+    private static final String user="postgres";
+    private static final String password="4938";
+
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection(url,url,password);
+        }
+        catch (SQLException e){
+            throw new DatabaseException("Connection failed",e);
+        }
+    }
+}
